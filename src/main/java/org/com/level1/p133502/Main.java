9ec8e133a01aab68b9d5b4efc837c95560640bd2
gkfktkrh153 <gkfktkrh153@naver.com>
2023-04-24 19:34:45 +0900

    햄버거 만들기(성능개선)

--- src/main/java/org/com/level1/p133502/Main.java
+++ src/main/java/org/com/level1/p133502/Main.java
@@ -1,5 +1,6 @@
 package org.com.level1.p133502;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,41 +12,26 @@ public class Main {
 }
 class Solution {
     public int solution(int[] ingredient) {
-        List<Integer> lst
-                = Arrays.stream(ingredient)
-                .boxed()
-                .collect(Collectors.toList());
-
-        int current = 0; // 현재 위치
-        int count = 0;
         int answer = 0;
-        int before = 0;
-        while(true){
-            if (lst.get(current) == 1) { // 내가 빵이라면
-                if (current - 3 >= 0){
-                    int index = current - 3;
-                    boolean flag = true;
-                    for(int i = 1; i <= 3; i++){
-                        if (lst.get(index) != i) // 순서에 맞지 않다면
-                            flag = false;
-                        index += 1;
-                    }
-                    if (flag == true) { // 햄버거 완성
-                        answer += 1;
-                        index -= 3;
-                        for (int i = 0; i < 4; i++)
-                            lst.remove(index);
-                        current -= 4;
-                    }
+        ArrayList<Integer> lst = new ArrayList<>();
 
+
+        for(int i : ingredient){
+            lst.add(i);
+
+            if (lst.size() >= 4){
+                if(lst.get(lst.size() -1)  == 1 &&
+                   lst.get(lst.size() -2)  == 3 &&
+                   lst.get(lst.size() -3)  == 2 &&
+                   lst.get(lst.size() -4)  == 1)
+                {
+                    for (int k = 0; k < 4; k++)
+                        lst.remove(lst.size()-1);
+                    answer += 1;
                 }
+
             }
-            if (current == lst.size() - 1)
-                break;
-            current += 1;
         }
-
-
 
         return answer;
     }
