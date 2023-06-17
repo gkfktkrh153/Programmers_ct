56beb9e8076e5665cb34f63fa1482a39b06d61fb
gkfktkrh153 <gkfktkrh153@naver.com>
2023-06-18 00:17:24 +0900

    PCCP 2번 외톨이 알파벳(순열로만)

--- src/main/java/org/com/pccp/p2/Main.java
+++ src/main/java/org/com/pccp/p2/Main.java
@@ -1,94 +1,37 @@
-package org.com.pccp.p2;
-
-import java.lang.reflect.Member;
-import java.util.ArrayList;
 import java.util.Stack;
-import java.util.logging.XMLFormatter;
 import java.util.Arrays;
-public class Main {
-    static Integer[] member;
-    static Integer[] sports;
-    static boolean[] visitedMember;
-    static boolean[] visitedSports;
-    static int[][] ability = {{40, 10, 10}, {20, 5, 0}, {30, 30, 30}, {70, 0, 70}, {100, 100, 100}};
-    static int max_sum = 0;
+class Solution {
     public static void main(String[] args) {
-        ability = new int[ability.length][ability[0].length];
 
-        member = new Integer[ability.length];
-        for (int i = 0; i < ability.length; i++)
-            member[i] = i;
+    }
+    static int max_sum = 0;
+    static boolean[] visitedMember;
+    public int solution(int[][] ability) {
+
         visitedMember = new boolean[ability.length];
+        permutation(ability[0].length, 0, ability, 0); // 종목 수 만큼 학생을 고름
 
-        combination(new Stack<>(), 3, 0);
-        System.out.println(max_sum);
+        return max_sum;
+
     }
 
 
-    public static void combination(Stack<Integer> stack, int r, int depth) {
-        if (stack.size() == r) {
-            System.out.println("member: " + stack.toString());
-            sports = new Integer[stack.size()];
-            for (int i = 0; i < stack.size(); i++)
-                sports[i] = stack.get(i);
-            visitedSports = new boolean[sports.length];
-
-            permutation(new Stack<>(), 3, 0);
-            return;
-        }
-
-        for (int i = depth; i < member.length; i++) {
-            if (visitedMember[i] == false) {
-                stack.push(member[i]);
-                visitedMember[i] = true;
-                combination(stack, r, i);
-                visitedMember[i] = false;
-                stack.pop();
-
-            }
-        }
-    }
-
-    public static void permutation(Stack<Integer> stack, int r, int depth) {
-        if (stack.size() == r) {
-            int sum = 0;
-            System.out.println(stack.toString());
-            for (int i = 0; i < stack.size(); i++) {
-                sum += ability[stack.get(i)][i];
-            }
+    public static void permutation(int r, int depth, int[][] ability,int sum) {
+        if (depth == r) {
             if(max_sum < sum)
                 max_sum = sum;
 
             return;
         }
 
-        for (int i = 0; i < sports.length; i++) {
-            if (visitedSports[i] == false) {
-                stack.push(sports[i]);
-                visitedSports[i] = true;
-                permutation(stack, r, depth + 1);
-                visitedSports[i] = false;
-                stack.pop();
+        for (int i = 0; i < ability.length; i++) {
+            if (visitedMember[i] == false) {
+                visitedMember[i] = true;
+                permutation(r, depth + 1, ability, sum + ability[i][depth]);
+                visitedMember[i] = false;
 
             }
         }
     }
+
 }
-/*    combination 0 | for
-    combination 1 | for 1
-    1 2
-    1 3
-    combination 2 return
-
-    combination 1 | for 2
-    2 1
-    2 3
-    combination 2 return
-
-        combination 1 | for 2
-    3 1
-    3 2
-    combination 2 return
-*/
-
-
